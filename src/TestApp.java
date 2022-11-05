466e4da7adf390379454804af4f18ccf2a131db5
Viktoriia <izhovska.viktoriia@gmail.com>
2022-11-05 05:11:51 +0200

    commit 3 made email public

--- src/TestApp.java
+++ src/TestApp.java
@@ -2,5 +2,6 @@ public class TestApp {
     public static void main(String[] args) {
         System.out.println("Something");
         System.out.println("Something2");
+        System.out.println("Made email public");
     }
 }
